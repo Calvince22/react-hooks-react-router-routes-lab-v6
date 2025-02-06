71db719d463f92607536ecba9e61963ea65c1f13
Caleb Kiptoo <vincentcalebkip@gmail.com>
2025-02-06 19:40:59 +0300

    made changes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,13 +1,19 @@
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-function MovieCard({title}) {
-  
+function MovieCard({ movie }) {
   return (
-    <article>
-        <h2>{title}</h2>
-        {/* What should go here? */}
-    </article>
+    <div className="movie-card">
+      <h2>{movie.title}</h2>
+      <p>Duration: {movie.time} mins</p>
+      <div>
+        Genres: {movie.genres.map((genre, index) => (
+          <span key={index}>{genre}{index < movie.genres.length - 1 ? ', ' : ''}</span>
+        ))}
+      </div>
+      {/* ✅ Add Link to Movie Details */}
+      <Link to={`/movie/${movie.id}`}>View Details</Link>  
+    </div>
   );
-};
+}
 
 export default MovieCard;
